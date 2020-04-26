33f03941e5b1d8d29406f4c8d88ea5d6ab94c726
Rafael Alexandre da Silva Prates <fc49449@alunos.fc.ul.pt>
2020-04-26 22:12:02 +0100

    SpellListManager: added a list of spells asset and the fireball spell to it

---
+++ Assets/Scripts/Spells/SpellListManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spell Manager", menuName = "Spells/Spell Manager")]
+public class SpellListManager : ScriptableObject
+{
+
+    [SerializeField] private List<Spell> SpellMap = new List<Spell>();
+
+    public Spell GetSpell(string gestureName)
+    {
+        return SpellMap.Find(spell => spell.GestureName.Equals(gestureName));
+    }
+}
