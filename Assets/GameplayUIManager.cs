84713ca0e2b0782b6ca9ea9ef3269b459430eaeb
lindacnatt <linda.cnattingius@gmail.com>
2020-04-30 15:43:12 +0200

    functioning health and ink bars

--- Assets/GameplayUIManager.cs
+++ Assets/GameplayUIManager.cs
@@ -8,15 +8,19 @@ public class GameplayUIManager : MonoBehaviour
     [SerializeField] private Text UIHPText = null;
     [SerializeField] private Text UIInkText = null;
     [SerializeField] private Text UISpellText = null;
+    [SerializeField] private Slider HealthSlider = null;
+    [SerializeField] private Slider InkSlider = null;
     
     public void UpdateHp(float newHP)
     {
         UIHPText.text = "Health: " + newHP;
+        HealthSlider.value = newHP;
     }
 
     public void UpdateInk(float newInk)
     {
         UIInkText.text = "Ink: " + newInk;
+        InkSlider.value = newInk;
     }
 
     public void UpdateSpellPrimed(Spell spell)
