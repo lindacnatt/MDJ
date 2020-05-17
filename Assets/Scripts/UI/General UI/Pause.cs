d5ee95d8f6e1d29dc43ed1470a2a9c064ec0c291
lindacnatt <linda.cnattingius@gmail.com>
2020-05-17 22:11:34 +0200

    add pause screen

---
+++ Assets/Scripts/UI/General UI/Pause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public Button open;
+    public Button close;
+   
+    public void PauseGame(){
+        Time.timeScale = 0;
+        open.enabled = false;
+        GetComponent<Canvas>().enabled = true;
+    }
+    public void ResumeGame(){
+       Time.timeScale = 1; 
+       open.enabled = true;
+       GetComponent<Canvas>().enabled = false;
+    }
+
+    
+}
