539dbc574a02b8ceb4e2a87d4703308d47811963
Rafael Alexandre da Silva Prates <fc49449@alunos.fc.ul.pt>
2020-05-19 20:38:07 +0100

    Spells: added AOE thunder spell

---
+++ Assets/Scripts/Spells/AOESpell.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AOESpell", menuName = "Spells/AOE Damaging Spell")]
+public class AOESpell : DamagingSpell
+{
+    //In game units
+    public float radius;
+    
+    //In seconds
+    public float delay;
+}
---
+++ Assets/Scripts/Spells/Thunder.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : MonoBehaviour, ISpell
+{
+
+    [SerializeField] private AOESpell AOESpellSettings = null;
+    [SerializeField] private CircleCollider2D Hurtbox = null;
+    [SerializeField] private SpriteRenderer BaseSprite = null;
+
+    Vector3 Center;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ActivateHurtbox());
+        StartCoroutine(IncreaseAlpha());
+
+        //Stay for extra time in the ground and then destroy itself
+        StartCoroutine(Death());
+
+    }
+
+    IEnumerator Death()
+    {
+        yield return new WaitForSecondsRealtime(AOESpellSettings.delay + 1.0f);
+        Destroy(gameObject);
+    }
+
+    IEnumerator IncreaseAlpha()
+    {
+        float elapstedTime = 0;
+        Color temp = BaseSprite.color;
+
+        while(elapstedTime < AOESpellSettings.delay)
+        {
+            //TODO: magic constants looool
+            //0 at start, 60 at end. 
+
+            float t = (elapstedTime / AOESpellSettings.delay);
+
+            //Exponential lerp
+            //temp.a = Mathf.SmoothStep(0, 60, t * t);
+            temp.a = LerpEaseInExpo(0, 60, t);
+
+            BaseSprite.color = temp;
+
+            elapstedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        //Make sure we finish at 60f alpha
+        temp.a = 60f;
+        BaseSprite.color = temp;
+    }
+
+    IEnumerator ActivateHurtbox()
+    {
+        yield return new WaitForSecondsRealtime(AOESpellSettings.delay);
+        Hurtbox.enabled = true;        
+    }
+
+    public bool OnSpellPrimed()
+    {
+        return false;
+    }
+
+    public void SetDestination(Vector2 target)
+    {
+        Vector3 temp = new Vector3(target.x, target.y, 0);
+        Center = temp;
+
+        transform.position = Center;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyController2D>().TakeDamage(AOESpellSettings.damage);
+            Destroy(gameObject);
+
+        }
+    }
+
+    static float LerpEaseInExpo(float from, float to, float t)
+    {
+        return ExpoEaseIn(t, from, to - from, 1.0f);
+    }
+
+
+    public static float ExpoEaseIn(float current, float initalValue, float totalChange, float duration)
+    {
+        return (current == 0) ? initalValue : totalChange * (float)Mathf.Pow(2, 10 * (current / duration - 1)) + initalValue - totalChange * 0.001f;
+    }
+
+}
